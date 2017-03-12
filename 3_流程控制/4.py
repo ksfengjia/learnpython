0c794cea2ca6d89bc13073c9c5197cbe3081174d
ksfengjia <ksfengjia@stu.xjtu.edu.cn>
2017-03-12 15:50:01 +0800

    学习到11课时，for循环

---
+++ 3_流程控制/4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+def fun():
+    return 0
+
+x = int(raw_input("please input x:"))
+y = int(raw_input("please input y:"))
+
+if not fun():
+    print "right"
+else:
+    print "wrong"
+
